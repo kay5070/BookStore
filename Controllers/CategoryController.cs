92ef4856b4b7aecfcd48f54b7fc2317039fc860f
kay5070 <karemsabry5@gmail.com>
2023-01-10 16:55:28 +0200

    Added All Action Methods With Views

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -32,9 +32,71 @@ namespace CuriousMindsBookstore.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null ||  id == 0)
+            {
+                return NotFound();
+
+            }
+            var categoryFrombDb= _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (categoryFrombDb == null)
+            {
+                return NotFound();
+
+            }
+            return View(categoryFrombDb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            var categoryFrombDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (categoryFrombDb == null)
+            {
+                return NotFound();
+
+            }
+            return View(categoryFrombDb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+              _db.Categories.Remove(obj);
+              _db.SaveChanges();
+              return RedirectToAction("Index");
+            
         }
     }
 }
